3fe8f8edc8ea96e983ceef60a4292a82649c46ba
Martin Sione <martinsione03@gmail.com>
2022-04-08 17:12:54 -0300

    Add dark theme support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
