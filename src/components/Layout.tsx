ad0ee865e1cc9e253846663fe7280b7beac092ee
Martin Sione <martinsione03@gmail.com>
2022-04-08 19:42:25 -0300

    Change selection color

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: Props) {
       <div className="relative z-10 mx-auto flex w-full sm:max-w-4xl">
         <span className="pointer-events-none absolute top-[-60vw] left-0 right-0 bottom-[-6vw] -z-10 bg-gradient-to-r from-rose-400/50 via-fuchsia-500/50 to-indigo-500/50 blur-3xl filter dark:from-rose-400/20 dark:via-fuchsia-500/20 dark:to-indigo-500/20" />
       </div>
-      <div className="min-h-screen antialiased selection:bg-pink-200/70 dark:text-gray-50 dark:selection:bg-pink-500">
+      <div className="min-h-screen antialiased selection:bg-pink-200/70 dark:text-gray-50 dark:selection:bg-pink-300 dark:selection:text-white">
         <main className="mx-auto w-full max-w-2xl py-8 px-6">{children}</main>
       </div>
     </>
