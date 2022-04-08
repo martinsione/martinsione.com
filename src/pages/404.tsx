dcb0393023a5761182bc408f6e6e1a12c44f006e
Martin Sione <martinsione03@gmail.com>
2022-04-08 19:39:32 -0300

    Add 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="mt-36 flex flex-col items-center justify-center space-y-8 md:mt-52">
+      <h4 className="text-3xl font-bold sm:text-5xl">404 - Not Found</h4>
+      <Link href="/">
+        <a className="flex items-center justify-center gap-2 rounded-lg bg-gradient-to-tl from-purple-500/80 to-rose-400/80 py-3 px-8 font-medium text-rose-100 duration-300  hover:scale-105 hover:shadow-md focus:outline-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-rose-500/70 active:scale-100">
+          Return home
+        </a>
+      </Link>
+    </div>
+  );
+}
