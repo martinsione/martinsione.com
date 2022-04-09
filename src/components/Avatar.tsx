c181c341895dc9052f3c0631f5b264f73f458154
Martin Sione <martinsione03@gmail.com>
2022-04-09 20:56:46 -0300

    Reduce avatar size

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -6,15 +6,15 @@ interface Props {
 }
 export default function Avatar({ alt, src }: Props) {
   return (
-    <div className="h-[96px] w-[96px] select-none rounded-full bg-gradient-to-tl from-purple-700/60 to-rose-400/60 p-1 shadow-lg sm:h-[160px] sm:w-[160px]">
+    <div className="h-[80px] w-[80px] select-none rounded-full bg-gradient-to-tl from-purple-700/60 to-rose-400/60 p-1 shadow-lg sm:h-[128px] sm:w-[128px]">
       <Image
         priority
         alt={alt}
         className="rounded-full"
         draggable="false"
-        height={160}
+        height={150}
         src={src}
-        width={160}
+        width={150}
       />
     </div>
   );
