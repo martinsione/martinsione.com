5db4e7025d756645266730ca8144b06b41bded2c
Martin Sione <martinsione03@gmail.com>
2022-04-09 17:02:21 -0300

    Add styles to blog

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,42 +1,58 @@
 import type { Post as IPost } from "contentlayer/generated";
 
+import { IoArrowBack } from "react-icons/io5";
+import Link from "next/link";
 import Image from "next/image";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { parseISO, format } from "date-fns";
 
 import { allPosts } from "contentlayer/generated";
+import ThemeButton from "@/components/ThemeButton";
 import components from "@/components/MDXComponents";
 
 export default function Post({ post }: { post: IPost }) {
   const Component = useMDXComponent(post.body.code);
   return (
-    <article className="mx-auto mb-16 flex w-full max-w-2xl flex-col items-start justify-center">
-      <h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
-        {post.title}
-      </h1>
-      <div className="mt-2 flex w-full flex-col items-start justify-between md:flex-row md:items-center">
-        <div className="flex items-center">
-          <Image
-            alt="Martin Sione"
-            className="rounded-full"
-            height={24}
-            src="/img/avatar.jpg"
-            width={24}
-          />
-          <p className="ml-2 text-sm text-gray-700 dark:text-gray-300">
-            {"Martin Sione / "}
-            {format(parseISO(post.date), "MMMM dd, yyyy")}
+    <>
+      <div className="mt-12 flex items-center justify-between">
+        <Link href="/">
+          <a className="flex h-9 w-9 items-center justify-center rounded-full bg-gray-200 text-2xl transition-all hover:ring-2 hover:ring-pink-300 dark:bg-neutral-700">
+            <IoArrowBack />
+          </a>
+        </Link>
+        <div>
+          <ThemeButton />
+        </div>
+      </div>
+      <article className="mx-auto mt-12 flex w-full max-w-3xl flex-col items-start justify-center">
+        <h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-5xl">
+          {post.title}
+        </h1>
+        <div className="mt-2 flex w-full flex-col items-start justify-between md:flex-row md:items-center">
+          <div className="flex items-center">
+            <Image
+              alt="Martin Sione"
+              className="rounded-full"
+              draggable="false"
+              height={24}
+              src="/img/avatar.jpg"
+              width={24}
+            />
+            <p className="ml-2 text-sm text-gray-700 dark:text-gray-300">
+              {"Martin Sione / "}
+              {format(parseISO(post.date), "MMMM dd, yyyy")}
+            </p>
+          </div>
+          <p className="min-w-32 mt-2 text-sm text-gray-600 dark:text-gray-400 md:mt-0">
+            {post.readingTime.text}
           </p>
         </div>
-        <p className="min-w-32 mt-2 text-sm text-gray-600 dark:text-gray-400 md:mt-0">
-          {post.readingTime.text}
-        </p>
-      </div>
-      <div className="dark:prose-dark prose mt-4 w-full max-w-none">
-        <Component components={components} />
-      </div>
-    </article>
+        <div className="prose mt-4 w-full max-w-none dark:prose-dark">
+          <Component components={components} />
+        </div>
+      </article>
+    </>
   );
 }
 
