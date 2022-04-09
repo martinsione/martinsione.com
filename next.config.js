fa416f9839c7029731386b470ee216ea7fe2e1dd
Martin Sione <martinsione03@gmail.com>
2022-04-09 01:52:43 -0300

    Add preact and reduce bundle size in ~50%

--- next.config.js
+++ next.config.js
@@ -20,6 +20,19 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
