c42f4caf524b2aedcc08db594e802c92c201dd75
Martin Sione <martinsione03@gmail.com>
2022-04-10 18:26:21 -0300

    Add sitemap and rss

---
+++ scripts/generate-rss.mjs
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-relative-packages, import/no-extraneous-dependencies  */
+import RSS from "rss";
+import { writeFileSync } from "fs";
+
+import { allPosts } from "../.contentlayer/generated/index.mjs";
+
+async function generate() {
+  const feed = new RSS({
+    title: "Martin Sione",
+    site_url: "https://martinsione.com",
+    feed_url: "https://martinsione.com/feed.xml",
+  });
+
+  allPosts
+    .map((post) => ({
+      title: post.title,
+      description: post.summary,
+      url: `https://martinsione.com/${post.slug}`,
+      date: post.date,
+    }))
+    .forEach((item) => {
+      feed.item(item);
+    });
+
+  writeFileSync("./public/feed.xml", feed.xml({ indent: true }));
+}
+
+generate();
---
+++ scripts/generate-sitemap.mjs
@@ -0,0 +1,48 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-relative-packages */
+import prettier from "prettier";
+import { globby } from "globby";
+import { writeFileSync } from "fs";
+
+async function generate() {
+  const prettierConfig = await prettier.resolveConfig("./.prettierrc.js");
+  const pages = await globby([
+    "pages/*.tsx",
+    "data/**/*.mdx",
+    "!data/*.mdx",
+    "!pages/_*.tsx",
+    "!pages/api",
+    "!pages/404.tsx",
+  ]);
+
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${pages
+          .map((page) => {
+            const path = page
+              .replace("pages", "")
+              .replace("data", "")
+              .replace(".tsx", "")
+              .replace(".mdx", "");
+            const route = path === "/index" ? "" : path;
+            return `
+              <url>
+                  <loc>${`https://martinsione.com${route}`}</loc>
+              </url>
+            `;
+          })
+          .join("")}
+    </urlset>
+    `;
+
+  const formatted = prettier.format(sitemap, {
+    ...prettierConfig,
+    parser: "html",
+  });
+
+  // eslint-disable-next-line no-sync
+  writeFileSync("public/sitemap.xml", formatted);
+}
+
+generate();
