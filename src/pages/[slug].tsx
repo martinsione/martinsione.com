d997ee1e1493f8146f99ae4d3a06e616837de939
Martin Sione <martinsione03@gmail.com>
2022-04-20 16:59:05 -0300

    Minor details in blog view

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,7 +1,7 @@
 import type { Post as IPost } from "contentlayer/generated";
 
 import { IoArrowBack } from "react-icons/io5";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaRegClock } from "react-icons/fa";
 import Link from "next/link";
 import { NextSeo } from "next-seo";
 import { useMDXComponent } from "next-contentlayer/hooks";
@@ -25,8 +25,9 @@ export default function Post({ post }: { post: IPost }) {
       />
       <div className="flex items-center justify-between sm:mt-12">
         <Link href="/">
-          <a className="flex h-9 w-9 items-center justify-center rounded-full bg-gray-200 text-2xl transition-all hover:ring-2 hover:ring-pink-300 dark:bg-gray-700">
-            <IoArrowBack />
+          <a className="flex items-center justify-center gap-2 rounded-full bg-gray-200 py-2 px-3 transition-all hover:ring-2 hover:ring-pink-300 dark:bg-gray-700 ">
+            <IoArrowBack className="text-lg" />
+            <p className="text-sm font-medium">martinsione.com</p>
           </a>
         </Link>
         <ThemeButton />
@@ -35,22 +36,25 @@ export default function Post({ post }: { post: IPost }) {
         <h1 className="mb-4 text-3xl font-bold tracking-tight text-black dark:text-white md:text-4xl">
           {post.title}
         </h1>
-        <div className="mt-2 flex w-full items-start space-x-2 text-sm text-gray-600 dark:text-gray-400">
+        <div className="mt-4 flex w-full items-start space-x-3 text-sm text-gray-600 dark:text-gray-400">
           <p>{format(parseISO(post.date), "MMMM dd, yyyy")}</p>
           <span>&middot;</span>
-          <p>{post.readingTime.text}</p>
+          <p className="flex items-center gap-2">
+            <FaRegClock />
+            {post.readingTime.text}
+          </p>
           <span className="hidden sm:block">&middot;</span>
           <a
-            className="hidden items-center gap-1 hover:text-gray-700 hover:underline hover:dark:text-gray-300 sm:flex"
+            className="hidden items-center gap-2 hover:text-gray-700 hover:underline hover:dark:text-gray-300 sm:flex"
             href={`https://github.com/martinsione/martinsione.com/edit/main/data/blog/${post.slug}.mdx`}
             rel="noopener noreferrer"
             target="_blank"
           >
-            <p>Edit on github</p>
             <FaGithub />
+            <p>Edit on github</p>
           </a>
         </div>
-        <div className="prose mt-4 w-full max-w-2xl dark:prose-dark">
+        <div className="prose mt-4 w-full max-w-none dark:prose-dark">
           <Component components={MDXComponents} />
         </div>
       </article>
